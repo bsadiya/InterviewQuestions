df4aab809e666b7c1a2f36cb746ff9c37c7f8e9e
= <sadbegum@deloitte.com>
2020-11-18 16:30:33 +0530

    "Added Few examples"

--- InterviewExamples/src/example/demo/string/StringManipulationDemo1.java
+++ InterviewExamples/src/example/demo/string/StringManipulationDemo1.java
@@ -3,15 +3,8 @@ package example.demo.string;
 public class StringManipulationDemo1 {
 
 	public static void main(String[] args) {
-		int a=10;
-		int b=20;
-		a= a+b;
-		b= a-b;
-		a= a-b;
-		System.out.println("a: "+a);
-		System.out.println("b: "+b);
 		
-//		String Swapping
+		
 
 	}
 
