8e04665f53c880f929e6d3487fcc9be80b1644c1
Sadiya <sadbegum@USHYDSADBEGUM01.us.deloitte.com>
2020-11-19 14:53:00 +0530

    Added Dup Element program

--- InterviewExamples/src/example/demo/arrays/DuplicateElements.java
+++ InterviewExamples/src/example/demo/arrays/DuplicateElements.java
@@ -1,9 +1,22 @@
 package example.demo.arrays;
 
+import java.util.Arrays;
+
 public class DuplicateElements {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] a= {10,20,30,40,10,60,90,30};
+		System.out.println(Arrays.toString(a));
+		
+		// Finding Duplicates
+		for(int i=0; i<a.length; i++) {
+			int count = 0;
+			for(int aa : a) {
+//				System.out.println("a[i]: "+ a[i] + "  And aa: "+aa);
+				if(a[i] == aa)count++;
+			}
+			System.out.println("a["+i+"] occurs: "+ count + " Times");
+		}
 
 	}
 
